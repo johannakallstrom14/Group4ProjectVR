6364face0b565e141ad0178a28a712b06da48f71
johannakallstrom14 <johannackallstrom@gmail.com>
2026-03-08 14:00:42 +0100

    Space-pond script and some changes

---
+++ Assets/Scripts/PondMaterialSwap.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PondMaterialSwap : MonoBehaviour
+{
+    [SerializeField] public Renderer pondRenderer;
+    [SerializeField] public Material normalMaterial;
+    [SerializeField] public Material magicalMaterial;
+
+    private bool hasChanged = false;
+
+    private void Reset()
+    {
+        pondRenderer = GetComponent<Renderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasChanged)
+        {
+            return;
+        }
+
+        //Check if the object touching the pond is a hand
+        if (other.CompareTag("Hand"))
+        {
+            hasChanged = true;
+            pondRenderer.material = magicalMaterial;
+            Debug.Log("Pond changed material");
+        }
+    }
+}
