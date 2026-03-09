3c03da953795465620a9403cc4387b680989a5f6
johannakallstrom14 <johannackallstrom@gmail.com>
2026-03-09 15:42:37 +0100

    Spacepond-collider

--- Assets/Scripts/PondMaterialSwap.cs
+++ Assets/Scripts/PondMaterialSwap.cs
@@ -5,24 +5,26 @@ public class PondMaterialSwap : MonoBehaviour
     [SerializeField] public Renderer pondRenderer;
     [SerializeField] public Material magicalMaterial;
     [SerializeField] public Transform fingertip;
-    [SerializeField] public float touchDistance = 0.05f;
+    [SerializeField] public BoxCollider triggerCollider;
+
+    [SerializeField] public float touchDistance = 0.15f;
 
 
     private bool hasChanged = false;
 
-    private void Reset()
+    /*private void Reset()
     {
         pondRenderer = GetComponent<Renderer>();
-    }
+    }*/
 
     private void Update()
     {
-        if(hasChanged || pondRenderer == null || fingertip == null)
+        if(hasChanged || pondRenderer == null || magicalMaterial == null || fingertip == null || triggerCollider == null)
         {
             return;
         }
 
-        Vector3 closestPoint = pondRenderer.bounds.ClosestPoint(fingertip.position);
+        Vector3 closestPoint = triggerCollider.ClosestPoint(fingertip.position);
         float distance = Vector3.Distance(fingertip.position, closestPoint);
 
         if(distance <= touchDistance)
